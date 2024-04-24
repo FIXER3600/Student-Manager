daced97cef20f27022cf7ebf3258e0b9d4e7bdad
FIXER3600 <guilhermecomercial51@gmail.com>
2024-04-24 16:03:50 -0300

    tela de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,200 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import {
+  Box,
+  Image,
+  Link as ChakraLink,
+  Text,
+  FormControl,
+  FormErrorMessage,
+  Center,
+  InputGroup,
+  InputRightElement,
+  Button,
+} from "@chakra-ui/react";
+import TextField from "../../components/TextField";
+import * as Yup from "yup";
+import { Formik, Form, Field } from "formik";
+import Eye from "../../assets/eye-open.png";
+import EyeClosed from "../../assets/eye-closed.png";
+import IconError from "../../assets/errorIcon.png";
+import {
+  Link as LinkSignup,
+  // useNavigate
+} from "react-router-dom";
+import React, { useState } from "react";
+const LoginSchema = Yup.object().shape(
+  {
+    email: Yup.string().when("password", {
+      is: (val: string) => !val?.length,
+      then: () =>
+        Yup.string().required("Todos os campos precisam ser preenchidos"),
+      otherwise: () => Yup.string().required("Campo obrigatório*"),
+    }),
+    password: Yup.string().when("email", {
+      is: (val: string) => !val?.length,
+      then: () =>
+        Yup.string().required("Todos os campos precisam ser preenchidos"),
+      otherwise: () => Yup.string().required("Campo obrigatório*"),
+    }),
+  },
+  [["email", "password"]]
+);
+
+const initialValues = {
+  email: "",
+  password: "",
+};
 
 function Login() {
+  // const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const handleSubmit = async (
+	//values
+) => {
+    //     try {
+    //       login(
+    //         {
+    //           email: values.email,
+    //           password: values.password,
+    //         },
+    //         navigate
+    //       );
+    //     } catch (error) {
+    //       console.error("Erro na requisição:", error);
+    //     }
+  };
   return (
-    <div>
-	<Box>
-		
-	</Box>
-    </div>
-  )
+    <Formik
+      validationSchema={LoginSchema}
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+     
+    >
+      {({ errors, touched, values }) => (
+        <Form  >
+       
+          <Box
+            display={"flex"}
+          w={"100vw"}
+            h={"100vh"}
+            backgroundColor={"#E3E3E3"}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+              
+            <Box
+              display={"flex"}
+              flexDirection={"column"}
+              alignItems={"center"}
+      
+            >
+ <Text fontSize='5xl' marginBottom={"20px"}>LOGIN</Text>
+              <FormControl>
+                {errors.password ===
+                "Todos os campos precisam ser preenchidos" ? (
+                  <Box display={"flex"} marginLeft={30} marginBottom={2}  >
+                    <Image src={IconError} marginRight={1} />
+                    <Text
+                      fontFamily={"Questrial"}
+                      fontSize={"12px"}
+                      color="red.500"
+                    >
+                      {errors.password}
+                    </Text>
+                  </Box>
+                ) : null}
+                <Box display={"flex"} justifyContent={"center"}>
+                <Field
+                      as={TextField}
+                  name="email"
+                  placeholder="Email"
+                  type="email"
+                  hasError={errors.email}
+                  isCheck={errors.email === undefined && values.email !== ""}
+                />
+                </Box>
+                <FormErrorMessage name="email" />
+                {errors.email === "Campo obrigatório*" && touched.email ? (
+                  <Text
+                    fontFamily={"Questrial"}
+                  
+                    color="red.500"
+                  >
+                    {errors.email}
+                  </Text>
+                ) : null}
+              </FormControl>
+              <br />
+              <FormControl>
+                <InputGroup display={"flex"} justifyContent={"center"}>
+                  <Field
+                        as={TextField}
+                    name="password"
+                    placeholder="Senha"
+                    hasError={errors.password && touched.password}
+                    isCheck={
+                      errors.password === undefined && values.password !== ""
+                    }
+                    type={showPassword ? "text" : "password"}
+                  />
+                  <InputRightElement
+                    width="auto"
+                   marginRight={"1em"}
+                    marginTop={"0.8em"}
+                  >
+                    {showPassword ? (
+                      <Image src={Eye} onClick={() => setShowPassword(false)} />
+                    ) : (
+                      <Image
+                        src={EyeClosed}
+                        onClick={() => setShowPassword(true)}
+                      />
+                    )}
+                  </InputRightElement>
+                </InputGroup>
+                <FormErrorMessage name="password" />
+                {errors.password === "Campo obrigatório*" &&
+                touched.password ? (
+                  <Text
+                    fontFamily={"Questrial"}
+                   
+                    color="red.500"
+                  >
+                    {errors.password}
+                  </Text>
+                ) : null}
+              </FormControl>
+
+              <Box
+                display={"flex"}
+                flexDirection={"row"}
+                justifyContent={"space-around"}
+                className="font-text"
+              >
+                <Box display={"flex"} paddingBottom={5} >
+                  <Text fontSize="25px" color={"#000"} marginRight={"8px"}>
+                    Ainda não tem uma conta?
+                  </Text>
+                  <ChakraLink
+                    fontSize="25px"
+                    color={"#3182CE"}
+                    fontWeight={400}
+                    as={LinkSignup}
+                    to="/signup"
+                  >
+                    Cadastre-se
+                  </ChakraLink>
+                </Box>
+              </Box>
+
+              <Center marginTop={5} className="font-text">
+                <Button size='lg' type={"submit"}>Entrar</Button>
+              </Center>
+            </Box>
+          </Box>
+        </Form>
+      )}
+    </Formik>
+  );
 }
 
-export default Login
+export default Login;
